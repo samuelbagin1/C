66579e687e38797dbb1a0cfe01a61d6e0617a7a6
samuelbagin1 <samuel.bagin1@gmail.com>
2024-03-01 08:51:28 +0100

    zadanie cislo 1

---
+++ hokus.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    char k;
+    while (k!=1) {
+        scanf("%c", &k);
+        printf("\n%c\n %d\n", k, k);
+    }
+    
+
+    return 0;
+}
